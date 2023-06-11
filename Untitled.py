26a6542ba8b3d9eec8aac78c074dbdbc85576fe1
ulatinaedupa <an94loo@gmail.com>
2023-06-10 19:40:46 -0500

    subiendo el lab 2
    se esta subiendo el laboratorio 2 para la tarea de esta semana

---
+++ Untitled.py
@@ -0,0 +1,10 @@
+import cv2
+face_cascade = cv2.CascadeClassifier(‘haarcascade_frontalface_default.xml’)
+cap = cv2.VideoCapture(“elon.jpg”)
+ret, img = cap.read()
+gray = cv2.cvtColor(img, cv2.COLOR_BR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+cv2.imshow(‘img’, img)
+cv2.waitKey(0)
+cap.release()
+cv2.destroyAllWindows()
